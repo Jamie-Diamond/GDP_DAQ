ca6d10576d382d3360362c36118fd5fa1cc5249b
Jamie Diamond <jamiediamond@me.com>
2016-11-12 13:37:10 +0000

    trying to get speed from access not going to well

--- Sensor_log.py
+++ Sensor_log.py
@@ -15,16 +15,16 @@ def sensor_log_lists(input):
                 Magnetic.append([float(b[3]), json.loads(b[2])])
             if 'Gyro' in a[0]:
                 b = a[0].split("|")
-                Gyro.append([float(b[3]), json.loads(b[2])])
+                Gyro.append([float(b[3])/1000, json.loads(b[2])])
             if 'GPS' in a[0]:
                 b = a[0].split("|")
-                GPS.append([float(b[3]), json.loads(b[2])])
+                GPS.append([float(b[3])/1000, json.loads(b[2])])
             if 'Acceleration' in a[0] and 'Linear' not in a[0]:
                 b = a[0].split("|")
-                Accel.append([int(b[3]), json.loads(b[2])])
+                Accel.append([int(b[3])/1000, json.loads(b[2])])
             if 'Linear Acceleration' in a[0]:
                 b = a[0].split("|")
-                Lin_Accel.append([float(b[3]), json.loads(b[2])])
+                Lin_Accel.append([float(b[3])/1000, json.loads(b[2])])
     GPS = GPS_Data_Tidy(GPS)
     print('Magn Data points:', len(Magnetic))
     freq_out(Magnetic)
@@ -41,12 +41,12 @@ def sensor_log_lists(input):
 def freq_out(data):
     from statistics import mean
     gap = []
-    if len(data) < 101:
+    if len(data) < 501:
         print('Data to short')
         return None
-    for i in range(100):
-        t0 = (data[i][0])/1000
-        t1 = (data[i+1][0])/1000
+    for i in range(500):
+        t0 = (data[i][0])
+        t1 = (data[i+1][0])
         gap.append(t1-t0)
     ave = mean(gap)
     freq = 1/ave
@@ -57,13 +57,20 @@ def freq_out(data):
 def GPS_Data_Tidy(GPS):
     import utm
     new_GPS = []
+    idx = -1
     for i in GPS:
-        t = i[1]['mTime']
+        t = i[1]['mTime']/1000
         ac = i[1]['mAccuracy']
         lon = i[1]['mLongitude']
         lat = i[1]['mLatitude']
         [Easting, Northing, _, _] = utm.from_latlon(lat, lon)
-        new_GPS.append([t, {'Accuracy': ac, 'Easting': Easting, 'Northing': Northing}])
+        try:
+            if t != new_GPS[idx][0]:
+                new_GPS.append([t, {'Accuracy': ac, 'Easting': Easting, 'Northing': Northing}])
+                idx += 1
+        except IndexError:
+            new_GPS.append([t, {'Accuracy': ac, 'Easting': Easting, 'Northing': Northing}])
+            idx += 1
     return new_GPS
 
 
@@ -81,7 +88,7 @@ def Mag_Data_Tidy(Mag):
     return new_mag
 
 
-def XYZ_plot(data):
+def XYZ_plot(data, mag=False):
     import matplotlib.pyplot as plt
     time, x, y, z, mag = [], [], [], [], []
     for i in data:
@@ -89,16 +96,30 @@ def XYZ_plot(data):
         x.append(i[1][0])
         y.append(i[1][1])
         z.append(i[1][2])
-        mag.append((i[1][0]**2+i[1][1]**2+i[1][2]**2)**0.5)
+        if mag:
+            mag.append((i[1][0]**2+i[1][1]**2+i[1][2]**2)**0.5)
     plt.plot(time, x, 'rx-', label='X')
     plt.plot(time, y, 'bx-', label='Y')
     plt.plot(time, z, 'k*-', label='Z')
-    plt.plot(time, mag, 'g+-', label='Magnitude')
+    if mag:
+        plt.plot(time, mag, 'g+-', label='Magnitude')
     plt.legend()
     plt.xlabel("Time (unix seconds)")
     plt.ylabel("Force [N]")
     plt.grid(True)
-    plt.show()
+
+
+def Y_plot(data, idx=1):
+    import matplotlib.pyplot as plt
+    time,  y = [], []
+    for i in data:
+        time.append(i[0])
+        y.append(i[1][idx])
+    plt.plot(time, y, 'bx', label='Y')
+    plt.legend()
+    plt.xlabel("Time (unix seconds)")
+    plt.grid(True)
+    return time, y
 
 
 def Mag_plot(data):
@@ -112,7 +133,6 @@ def Mag_plot(data):
     plt.xlabel("UnixTime stamp [s]")
     plt.ylabel("Heading [º]")
     plt.grid(True)
-    plt.show()
 
 
 def GPS_plot(data):
@@ -125,17 +145,63 @@ def GPS_plot(data):
         north.append(i[1]['Northing'])
     plt.errorbar(east, north, xerr=acc, yerr=acc, label='GPS', ecolor='b', color='k', marker='o')
     plt.legend()
-    plt.xlabel("")
-    plt.ylabel("")
+    plt.xlabel("East [m]")
+    plt.ylabel("North [m]")
     plt.grid(True)
     plt.axis('equal')
+
+
+def GPS_speed_plot(data):
+    speeds = []
+    times = []
+    time = []
+    acc = []
+    east = []
+    north = []
+    import matplotlib.pyplot as plt
+    for i in data:
+        time.append(i[0])
+        acc.append(i[1]['Accuracy'] / 2)
+        east.append(i[1]['Easting'])
+        north.append(i[1]['Northing'])
+    for i in range(len(data)-1):
+        Ediff = east[i] - east[i+1]
+        Ndiff = north[i] - north[i+1]
+        hyp = (Ediff**2+Ndiff**2)**0.5
+        timediff = (time[i+1]-time[i])/1000
+        times.append(time[i])
+        speeds.append(hyp/timediff)
+    plt.plot(times, speeds)
+    plt.xlabel("UnixTime [s]")
+    plt.ylabel("Speed [m/s]")
+    plt.grid(True)
+
+
+def integrator(y, time):
+    import matplotlib.pyplot as plt
+    from scipy import integrate
+    import numpy as np
+    plt.subplot(2, 1, 1)
+    plt.plot(Time, y)
+    yint = integrate.cumtrapz(y, time, initial=0)
+    yint = np.ndarray.tolist(yint)
+    plt.subplot(2, 1, 2)
+    plt.plot(time, yint)
     plt.show()
+    return yint
+
+def movingaverage(interval, window_size):
+    import numpy
+    window = numpy.ones(int(window_size))/float(window_size)
+    temp = numpy.convolve(interval, window, 'same')
+    return numpy.ndarray.tolist(temp)
 
 
 if __name__ == "__main__":
-    [Mag, Gyro, GPS, Accel, Lin_Accel] = sensor_log_lists('sensor_log2.txt')
-    #GPS_plot(GPS)
-    Mag_plot(Mag)
-    # XYZ_plot(Lin_Accel)
-    # XYZ_plot(Accel)
+    [Mag, Gyro, GPS, Accel, Lin_Accel] = sensor_log_lists('Drive_log.txt')
+    import matplotlib.pyplot as plt
+    [Time, Y] = Y_plot(Lin_Accel)
+    Y = movingaverage(Y, 50)
+    integrator(Y, Time)
+
 

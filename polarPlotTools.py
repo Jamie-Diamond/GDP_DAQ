edc75dd53f002d0fa61b08d260424f4742440490
Jamie Diamond <jamiediamond@me.com>
2016-11-28 11:05:14 +0000

    Updated to new naming convention, will make a name convention table soon to explain what each thing is

--- polarPlotTools.py
+++ polarPlotTools.py
@@ -12,13 +12,14 @@ def addApparentWind(GPSWindHead):
     index = 0
     for i in GPSWindHead:
         if index == 0:
-            i[1]['App Wind Dir'] = None
-            i[1]['App Wind Speed'] = None
+            i[1]['AWA'] = None
+            i[1]['AWS'] = None
+            i[1]['TWA'] = None
         else:
             try:
-                i[1]['Wind Dir'] = i[1]['Heading'] - i[1]['Wind Dir']
-                i[1]['App Wind Dir'] = None ####################### Add Equation here ########################
-                i[1]['App Wind Speed'] = None#i[1]['Wind Speed']*cos(i[1]['App Wind Dir'])
+                i[1]['TWA'] = i[1]['Heading'] - i[1]['Wind Dir']
+                i[1]['AWA'] = None ####################### Add Equation here ########################
+                i[1]['AWS'] = None#i[1]['Wind Speed']*cos(i[1]['App Wind Dir'])
             except KeyError:
                 print('Something not found in: ' + str(i))
 
@@ -31,44 +32,48 @@ def addApparentWind(GPSWindHead):
     print('\r\nComplete.\r\n')
     return GPSWindHead
 
-def speed_plot(Data):
-    speed = []
+def linar_var_plot(Data, key='Speed',windSpeed=15, error=15):
+    var = []
     time = []
     for i in Data:
-        speed.append(i[1]["Speed"])
-        time.append(i[0])
-    matplotlib.pyplot.plot(time, speed,'x')
+        if i[1]["Wind Speed"] != None:
+            temp = abs(i[1]["Wind Speed"] - windSpeed)
+            if temp < error:
+                var.append(i[1][key])
+                time.append(i[0])
+    matplotlib.pyplot.plot(time, var,'x')
     #matplotlib.pyplot.show()
 
 
-
-def plotPolars(Data, windSpeed=0, error=360):
+def plotPolars(Data, windSpeed=15, error=15):
     theta = []
     r = []
-
     for i in Data:
         if i[1]["Wind Speed"] != None:
             var = abs(i[1]["Wind Speed"] - windSpeed)
             if var < error:
-                theta.append(i[1]["Wind Dir"])
+                theta.append(i[1]["TWA"])
                 r.append(i[1]["Speed"])
 
     axis = matplotlib.pyplot.subplot(111, projection='polar')
     axis.set_theta_zero_location('N')
     axis.set_theta_direction(-1)
     axis.set_rlabel_position(90)
-    axis.plot(theta, r, 'rx')
+    axis.plot(theta, r, '+b')
     axis.grid(True)
-
     axis.set_title("Boat Speed vs Wind Direction (Wind Speed: " + str(windSpeed) + "±" + str(error) + ")", va='bottom')
     matplotlib.pyplot.show()
+
+
 
 Mag, Gyro, GPS, Accel, Lin_Accel = sensor_log_read('log.txt')
 GPSWind = getWindData(GPS)
 GPSWindHead = addSpeedAndDirToGPS(GPSWind, Mag)
-speed_plot(GPSWindHead)
-matplotlib.pyplot.figure(2)
+
+
 GPSWindAHead = addApparentWind(GPSWindHead)
-plotPolars(GPSWindAHead, 13, 5)
+linar_var_plot(GPSWindAHead,'TWA', 13, 1)
+matplotlib.pyplot.figure(2)
+plotPolars(GPSWindAHead, 13, 1)
 
 

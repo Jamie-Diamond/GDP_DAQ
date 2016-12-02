d179a49d4ae0330a879b65a5a69122951ed175ae
Jamie Diamond <jamiediamond@me.com>
2016-12-02 16:43:30 +0000

    viewer speed ups - still way to slow

--- Viewer_mk1.py
+++ Viewer_mk1.py
@@ -11,22 +11,32 @@ def viewer1():
 
     fin = len(data)-1
     freq = 10/(data[100][0] - data[90][0])
-    idx = 0
+    idx = 500
     leng = 180
     samples = int(freq * leng-1)
+    N, E, A = [0], [0], [0]
+    cog, cogt, cow, cowt = [0], [0], [0], [0]
+    hdg, hdgt, sog, sogt = [0], [0], [0], [0]
+    bsp, bspt = [], []
     while idx < fin-samples:
-
         idxS = idx
         idxF = idx+samples
         T1 = data[idxS][0]
         T2 = data[idxF][0]
-        data_trim = data_time_trim(data, T1, T2)
-        GPS_plot(data_trim, pause=0.00000000001)
-        linar_var_plot(data_trim,['SOG', 'BSP', 'TDS', 'AWS'], pause=0.000000000001, fig=8)
-        linar_var_plot(data_trim, ['COG', 'HDG', 'COW'], pause=0.0000000000001, fig=9)
-        idx += 500
-    #
-    # from Plotting_ToolBox import linar_var_plot, GPS_plot
+        N, E, A = GPS_slim_data_retrive(data, idxS, idxF, N, E, A)
+        slimline_GPS_plot(N, E, A, pause=0.0000001, fig=124)
+        slim_data_retrive(data, idxS, idxF, cog, cogt, 'COG')
+        slim_data_retrive(data, idxS, idxF, hdg, hdgt, 'HDG')
+        slim_data_retrive(data, idxS, idxF, cow, cowt, 'COW')
+        slim_data_retrive(data, idxS, idxF, sog, sogt, 'SOG')
+        slim_data_retrive(data, idxS, idxF, bsp, bspt, 'BSP')
+        slimline_linar_var_plot([cog, cow, hdg], [cogt, cowt, hdgt], ['COG', 'COW', 'HDG'], pause=0.0000001)
+        slimline_linar_var_plot([sog, bsp], [sogt, bspt], ['SOG', 'BSP'], pause=0.0000001, fig=1)
+        idx += 4
+
+
+
+
     # # good upwind segment here
     # data = data_time_trim(data, 1479042400, 1479043500)
     # linar_var_plot(data, ['GWD', 'COG', 'TWA', 'TWD'])
@@ -35,3 +45,64 @@ def viewer1():
     # GPS_plot(data)
     # import matplotlib.pyplot
     # matplotlib.pyplot.show()
+
+
+def slim_data_retrive(data, idx_s, idx_f, mag, time, key):
+    idx = idx_s
+    length = idx_f - idx_s
+    while idx < idx_f:
+        mag.append(data[idx][1][key])
+        time.append(data[idx][0])
+        if len(mag) > length:
+            mag.pop(0)
+            time.pop(0)
+        idx += 1
+    return mag, time
+
+
+
+def slimline_linar_var_plot(mag, times, key, pause=False, fig=111, ):
+    import matplotlib.pyplot as plt
+    plt.figure(fig)
+    plt.gcf().clear()
+    idx = 0
+    for i in key:
+        var = mag[idx]
+        time = times[idx]
+        plt.plot(time, var, '-', label=i)
+        plt.legend()
+        idx += 1
+    if type(pause) is float:
+        plt.pause(pause)
+
+
+def GPS_slim_data_retrive(data, idx_s, idx_f, N, E, A):
+    idx = idx_s
+    length = idx_f - idx_s
+    while idx < idx_f:
+        N.append(data[idx][1]['Northing'])
+        E.append(data[idx][1]['Easting'])
+        A.append(data[idx][1]['Accuracy']/2)
+        if len(N) > length:
+            N.pop(0)
+            E.pop(0)
+            A.pop(0)
+        idx += 1
+    return N, E, A
+
+def slimline_GPS_plot(N, E, A, pause=False, fig=123):
+    '''Plots GPS data'''
+    import matplotlib.pyplot as plt
+    plt.figure(fig)
+    plt.gcf().clear()
+    plt.errorbar(E, N, xerr=A, yerr=A, label='GPS', ecolor='b', color='k', marker='o')
+    plt.legend()
+    plt.xlabel("East [m]")
+    plt.ylabel("North [m]")
+    plt.grid(True)
+    plt.axis('equal')
+    if type(pause) is float:
+        plt.pause(pause)
+
+
+viewer1()
